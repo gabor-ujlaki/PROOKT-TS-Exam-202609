726ac621d3c2e4e7cdc7b74d35095a521f298fa9
gabor-ujlaki <gabor.ujlaki@gmail.com>
2026-09-14 20:10:40 +0200

    change Participant class

---
+++ src/models/Participant.ts
@@ -0,0 +1,50 @@
+import { IBaseParticipant } from './interfaces/IBaseParticipant';
+import { ParticipantRole } from './EventTypes';
+
+export class Participant implements IBaseParticipant {
+    private _id: string;
+    private _name: string;
+    private _email: string;
+    private _role: ParticipantRole;
+
+    constructor(id: string, name: string, email: string, role: ParticipantRole) {
+        this._id = id;
+        this._name = name;
+        this._email = email;
+        this._role = role;
+    }
+
+    public get id(): string {
+        return this._id;
+    }
+
+    public get name(): string {
+        return this._name;
+    }
+
+    public set name(newName: string) {
+        this._name = newName;
+    }
+
+    public get email(): string {
+        return this._email;
+    }
+
+    public set email(newEmail: string) {
+        this._email = newEmail;
+    }
+
+    public get role(): ParticipantRole {
+        return this._role;
+    }
+
+    public set role(newRole: ParticipantRole) {
+        this._role = newRole;
+    }
+
+    public print(): void {
+        console.log(
+            `ID: ${this._id} | Név: ${this._name} | Email: ${this._email} | Szerep: ${this._role}`
+        );
+    }
+}
